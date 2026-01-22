101072ca4388d79bf7cdb769490b0c50bcd3e9a2
Dinael Domingos <dinael@uol.com.br>
2026-01-21 21:48:41 -0300

    Criação do exercício 4.3;

---
+++ Exercicios/cap04/exercicio4_3/exercicio4_3.js
@@ -0,0 +1,34 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const valor = Number(frm.inValor.value)
+    let tempo
+    let troco
+    if (valor >= 3) {
+        tempo = 120
+        troco = valor - 3
+        resp1.textContent = `Tempo: ${tempo} minutos`
+        resp2.textContent = `Troco: R$ ${troco.toFixed(2)}`
+    } else if (valor >= 1.75) {
+        tempo = 60
+        troco = valor - 1.75
+        resp1.textContent = `Tempo: ${tempo} minutos`
+        resp2.textContent = `Troco: R$ ${troco.toFixed(2)}`
+    } else if (valor >= 1) {
+        tempo = 30
+        troco = valor - 1
+        resp1.textContent = `Tempo: ${tempo} minutos`
+        resp2.textContent = `Troco: R$ ${troco.toFixed(2)}`
+    } else {
+        resp1.textContent = "Valor insuficiente"
+        resp2.textContent = ""
+    }
+})
+
+frm.addEventListener("reset", () => {
+    resp1.textContent = ""
+    resp2.textContent = ""
+})
